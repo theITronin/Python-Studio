e198d987d36a9c313f18845095b0fc3e7a33cd50
theITronin <diegobustamanl@hotmail.com>
2026-07-01 14:47:08 +0200

    Update comments for string slicing examples

--- Scripts_Notes/SIMIR_python/script7_slicing.py
+++ Scripts_Notes/SIMIR_python/script7_slicing.py
@@ -10,7 +10,7 @@
 # using slicing (rebanadas)
 s = "Métodos útiles"
 # index: 0 1 2 3 4 5 6 7 8 9 10 11 12 13
-#          M é t o d o s   ú t  i  l  e  s
+#        M é t o d o s   ú t  i  l  e  s
 
 print(s[0:7])  # "Métodos"
 print(s[:7])   # "Métodos"
